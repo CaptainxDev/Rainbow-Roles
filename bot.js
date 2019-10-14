01011c17d069342f0907c28e26ee51ea7334b7e2
onelitghost <49221487+onelitghost@users.noreply.github.com>
2019-10-14 21:37:01 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,6 +33,7 @@ client.on('ready', () => {
   console.log('╔[════════════]╗')
   console.log('Created by 아유 | Aka ＡＹＵＳＨ#1999')
   console.log('╚[════════════]╝')
+	client.user.setActivity('Team Color OS', {type: "playing"})
  
 console.log('Done The Watching Setup Completed')
 	
@@ -41,12 +42,12 @@ console.log('Done The Watching Setup Completed')
 client.on('message', message => {//new msg event
 if(!message.channel.guild) return;
   if(message.content.startsWith(prefix + 'setup')) {//to create the rainbow role
-	  let role = message.guild.roles.find('name', 'Team Color OS')
+	  let role = message.guild.roles.find('name', 'TCOS | ☆ Team Color OS')
     if(role) return message.channel.send(`This Setup is Already Completed !`)//if the role already created return with this msg
   //start of create role 
   if(!role){
    rainbow = message.guild.createRole({
-   name: "Team Color OS",//the role will create name
+   name: "TCOS | ☆ Team Color OS",//the role will create name
    color: "#000000",//the default color
    permissions:[]//the permissions
  //end of create role
@@ -61,7 +62,7 @@ message.channel.send('The Rainbow Role Has Successfully Set Up')//if the step co
 client.on('ready', () => {//new ready event
   setInterval(function(){
       client.guilds.forEach(g => { 
-                  var role = g.roles.find('name', 'Team Color OS');//rainbow role name
+                  var role = g.roles.find('name', 'TCOS | ☆ Team Color OS');//rainbow role name
                   if (role) {
                       role.edit({color : "RANDOM"});
                   };
