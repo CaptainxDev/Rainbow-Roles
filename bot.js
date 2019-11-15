14ac6eb22e1a9b6200156ca0b699a7d3498cbb3d
onelitghost <49221487+onelitghost@users.noreply.github.com>
2019-11-15 07:22:54 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ client.on("ready", () => {
         role.edit({ color: "RANDOM" });
       }
     });
-  }, 10000); //the rainbow time
+  }, 15000); //the rainbow time
 });
 
 client.login(process.env.TOKEN);
