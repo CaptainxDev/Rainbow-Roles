8774da792128cbfdc5aaa951de67ddf911f62895
onelitghost <49221487+onelitghost@users.noreply.github.com>
2019-12-09 12:59:41 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,6 +26,38 @@ client.once("ready", () => {
  
 });
 
+//Best Rainbow Bot .
+client.on("message", message => {
+  //new msg event
+  if (!message.channel.guild) return;
+  if (message.content.startsWith("!setup")) {
+    //to create the rainbow role
+    let role = message.guild.roles.find("name", "Rainbow");
+    if (role)
+      return message.channel.send({
+        embed: {
+          color: 0xff2222,
+          title:
+            "This Setup is Already Completed !"
+        }
+      }); //if the role already created return with this msg
+    //start of create role
+    if (!role) {
+      role = message.guild.createRole({
+        name: "Rainbow", //the role will create name
+        color: "#000000", //the default color
+        permissions: [] //the permissions
+        //end of create role
+      });
+    }
+    message.channel.send({
+      embed: {
+        color: 0x0efc3e,
+        title:
+          "The Rainbow Role Has Successfully Set Up"
+      }
+    }); //if the step completed
+  
 client.on("ready", () => {
   //new ready event
   setInterval(function() {
@@ -35,7 +67,7 @@ client.on("ready", () => {
         role.edit({ color: "RANDOM" });
       }
     });
-  }, 65000); //the rainbow time
+  }, 5000); //the rainbow time
 });
 
 client.login(process.env.TOKEN);
